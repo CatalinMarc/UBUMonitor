1cc12834e12b8dedc6561bde08296f1685a43931
Ionut Catalin Marc <catalinmarc17@gmail.com>
2024-07-01 00:22:06 +0200

    Export CSV data

--- src/main/java/es/ubu/lsi/ubumonitor/clustering/chart/MapScatter2D.java
+++ src/main/java/es/ubu/lsi/ubumonitor/clustering/chart/MapScatter2D.java
@@ -2,20 +2,26 @@ package es.ubu.lsi.ubumonitor.clustering.chart;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.ubu.lsi.ubumonitor.clustering.controller.Connector;
 import es.ubu.lsi.ubumonitor.clustering.controller.MapConnector;
 import es.ubu.lsi.ubumonitor.clustering.controller.MapsController;
 import es.ubu.lsi.ubumonitor.clustering.controller.PartitionalClusteringController;
+import es.ubu.lsi.ubumonitor.clustering.util.ExportUtil;
 import javafx.concurrent.Worker;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MapScatter2D extends AbstractChart {
 
 	private MapConnector connector;
+	
+	private String data;
 	
 	public MapScatter2D(MapsController mapsController) {
 		super(mapsController.getWebViewScatter());
@@ -34,7 +40,8 @@ public class MapScatter2D extends AbstractChart {
 	}
 	
 	public void updateChart(String data) {
-	
+		this.data = data;
+		
 		WebEngine webEngine = getWebEngine();
 
 		JSObject window = (JSObject) webEngine.executeScript("window");
@@ -45,7 +52,27 @@ public class MapScatter2D extends AbstractChart {
 	
 	@Override
 	protected void exportData(File file) throws IOException {
+		JSONObject jsonObject = new JSONObject(data);
 		
+		JSONArray xArray = jsonObject.getJSONArray("x");
+        JSONArray yArray = jsonObject.getJSONArray("y");
+        JSONArray labelsArray = jsonObject.getJSONArray("labels");
+		
+		String[] head = new String[] { "FullName", "X", "Y" };
+		List<List<Object>> fileData = new ArrayList<>();
+		
+		for (int i = 0; i < labelsArray.length(); i++) {
+			List<Object> row = new ArrayList<>();
+			
+			row.add(labelsArray.getString(i));
+			row.add(xArray.getDouble(i));
+			row.add(yArray.getDouble(i));
+			
+			
+			fileData.add(row);
+		}
+		
+		ExportUtil.exportCSV(file, head, fileData);
 	}
 
 }
--- src/main/java/es/ubu/lsi/ubumonitor/clustering/chart/MapScatter3D.java
+++ src/main/java/es/ubu/lsi/ubumonitor/clustering/chart/MapScatter3D.java
@@ -2,12 +2,17 @@ package es.ubu.lsi.ubumonitor.clustering.chart;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import es.ubu.lsi.ubumonitor.clustering.controller.Connector;
 import es.ubu.lsi.ubumonitor.clustering.controller.MapConnector;
 import es.ubu.lsi.ubumonitor.clustering.controller.MapsController;
 import es.ubu.lsi.ubumonitor.clustering.controller.PartitionalClusteringController;
+import es.ubu.lsi.ubumonitor.clustering.util.ExportUtil;
 import javafx.concurrent.Worker;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -16,6 +21,8 @@ import netscape.javascript.JSObject;
 public class MapScatter3D extends AbstractChart {
 
 	private MapConnector connector;
+	
+	private String data;
 	
 	public MapScatter3D(MapsController mapsController) {
 		super(mapsController.getWebView3DScatter());
@@ -34,6 +41,7 @@ public class MapScatter3D extends AbstractChart {
 	}
 	
 	public void updateChart(String data) {
+		this.data = data;
 		
 		WebEngine webEngine = getWebEngine();
 
@@ -46,7 +54,29 @@ public class MapScatter3D extends AbstractChart {
 	
 	@Override
 	protected void exportData(File file) throws IOException {
+		JSONObject jsonObject = new JSONObject(data);
 		
+		JSONArray xArray = jsonObject.getJSONArray("x");
+        JSONArray yArray = jsonObject.getJSONArray("y");
+        JSONArray zArray = jsonObject.getJSONArray("z");
+        JSONArray labelsArray = jsonObject.getJSONArray("labels");
+		
+		String[] head = new String[] { "FullName", "X", "Y", "Z" };
+		List<List<Object>> fileData = new ArrayList<>();
+		
+		for (int i = 0; i < labelsArray.length(); i++) {
+			List<Object> row = new ArrayList<>();
+			
+			row.add(labelsArray.getString(i));
+			row.add(xArray.getDouble(i));
+			row.add(yArray.getDouble(i));
+			row.add(zArray.getDouble(i));
+			
+			
+			fileData.add(row);
+		}
+		
+		ExportUtil.exportCSV(file, head, fileData);
 	}
 
 }
